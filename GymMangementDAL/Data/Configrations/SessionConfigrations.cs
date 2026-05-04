d88b06633292694b7dd94989a2b7211107c2e9aa
Youssef mohamed <youssefelbazz77@gmail.com>
2026-05-05 02:42:43 +0300

    DAL/AllConfigrations

--- GymMangementDAL/Data/Configrations/SessionConfigrations.cs
+++ GymMangementDAL/Data/Configrations/SessionConfigrations.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using GymMangementDAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,15 @@ using System.Threading.Tasks;
 
 namespace GymMangementDAL.Data.Configrations
 {
-    internal class SessionConfigrations
+    internal class SessionConfigrations : IEntityTypeConfiguration<Session>
     {
+        public void Configure(EntityTypeBuilder<Session> builder)
+        {
+            builder.ToTable(Tb =>
+            {
+                Tb.HasCheckConstraint("CK_Session_Capacity", "Capacity Between 1 and 25");
+                Tb.HasCheckConstraint("CK_Session_Dates", "StartDate < EndDate");
+            });
+        }
     }
 }
