62ff9a9548f710cbbb7d90a70c6ab42df1e8df85
Youssef mohamed <youssefelbazz77@gmail.com>
2026-05-07 10:17:17 +0300

    IMemberRepository

---
+++ GymMangementDAL/Repositories/Classes/MemberRepository.cs
@@ -0,0 +1,47 @@
+﻿using GymMangementDAL.Data.Contexts;
+using GymMangementDAL.Entities;
+using GymMangementDAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymMangementDAL.Repositories.Classes
+{
+    internal class MemberRepository : IMemberRepository
+    {
+        private readonly GymDbContext _dbContext= new GymDbContext();
+
+        public int Add(Member member)
+        {
+            _dbContext.Members.Add(member);
+            return _dbContext.SaveChanges();
+        }
+
+        public int Delete(int id)
+        {
+            var Member = _dbContext.Members.Find(id);
+            if(Member is null) { return 0; }
+            _dbContext.Members.Remove(Member);
+            return _dbContext.SaveChanges();
+        }
+
+        public IEnumerable<Member> GetAlL()
+        {
+           
+             return _dbContext.Members.ToList();
+        }
+
+        public Member? GetById(int id)
+        {
+            return _dbContext.Members.Find(id);
+        }
+
+        public int Update(Member member)
+        {
+            _dbContext.Members.Update(member);
+            return _dbContext.SaveChanges();
+        }
+    }
+}
